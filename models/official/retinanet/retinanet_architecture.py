4e4d55ebe7eb72315a67c9b53eb035152fcc2b46
BRougeHub <38994494+BRougeHub@users.noreply.github.com>
2019-03-12 16:01:11 -0500

    Update retinanet_architecture.py

--- models/official/retinanet/retinanet_architecture.py
+++ models/official/retinanet/retinanet_architecture.py
@@ -687,9 +687,8 @@ def retinanet(features,
                       kernel_initializer=tf.random_normal_initializer(stddev=0.01),
                       padding='same',
                       name='panoptic-final')
-      map_output = fused_feature
-      #map_output = resize_bilinear(fused_feature, tf.shape(features)[1:3],
-       #                               feats[level].dtype)  
+      map_output = resize_bilinear(fused_feature, tf.to_int32(tf.shape(features)[1:3]/2),
+                                      feats[level].dtype)  
       
 
   return class_outputs, box_outputs, map_output
